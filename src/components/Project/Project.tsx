93f97cc437c2d2fd1b59dce1e76f620ddd7427ed
kennedy-ben <makuanikennedy@gmail.com>
2024-02-06 12:03:19 +0300

    added new link for the projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -18,20 +18,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://kameleon.co.in/" target="_blank" rel="noreferrer">
+                <a href="https://bling-hotel.web.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>Kameleon - Cloth Selling Site</h3>
+              <h3>Bling  - Hotel Booking Site</h3>
               <p>
-              This website sells men's cloth products and helps men look their best. It is easy to find what you need, whether it's cloth shirts, pants, or accessories. The site is useful for men who want to look their best and need some help.
+                This is Hotel booking site for visitors or owners,Help to avoid last time rush in search of a room.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Shopify</li>
+                <li>Hotel System</li>
               </ul>
             </footer>
           </div>
